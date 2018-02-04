1c7f7808c7a29f420ea9c6b9690d93ba904a4ad8
Rolf Neugebauer <rolf.neugebauer@gmail.com>
2018-02-04 18:38:38 +0000

    gh-report: WIP: Extract Issues/PRs/Users and print them in markdown
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@gmail.com>

--- gh-report.go
+++ gh-report.go
@@ -5,11 +5,74 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"sort"
 	"strings"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
+
+// Item has some fields extracted from GitHub issue (PRs are issues too)
+type Item struct {
+	issue     *github.Issue
+	ID        string
+	PR        bool
+	State     string
+	Title     string
+	URL       string
+	CreatedBy *User
+}
+
+func newItem(issue *github.Issue, repo string) *Item {
+	item := &Item{issue: issue,
+		ID:    fmt.Sprintf("%s#%d", repo, *issue.Number),
+		PR:    issue.IsPullRequest(),
+		State: *issue.State,
+		Title: *issue.Title,
+		URL:   *issue.HTMLURL,
+	}
+
+	if issue.User != nil {
+		item.CreatedBy = newUser(issue.User)
+	}
+	return item
+}
+
+func (i *Item) String() string {
+	return fmt.Sprintf("%s ([%s] %s)", i.Title, i.ID, i.CreatedBy)
+}
+
+// Link returns a markdown style link to the item
+func (i *Item) Link() string {
+	return fmt.Sprintf("[%s]: %s", i.ID, i.URL)
+}
+
+// User is a structure with information about a user
+type User struct {
+	ID  string
+	URL string
+}
+
+var users map[string]*User
+
+// Create a new User if it is not in the map
+func newUser(u *github.User) *User {
+	if user, ok := users[*u.Login]; ok {
+		return user
+	}
+	user := &User{ID: *u.Login, URL: *u.HTMLURL}
+	users[user.ID] = user
+	return user
+}
+
+func (u *User) String() string {
+	return "[@" + u.ID + "]"
+}
+
+// Link returns a markdown style link to the item
+func (u *User) Link() string {
+	return fmt.Sprintf("[@%s]: %s", u.ID, u.URL)
+}
 
 func main() {
 	accessToken := flag.String("token", "", "GitHub access token")
@@ -29,24 +92,69 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	for _, owner_repo := range repos {
-		t := strings.SplitN(owner_repo, "/", 2)
+	users = make(map[string]*User)
+	prs := make(map[string]*Item)
+	issues := make(map[string]*Item)
+
+	for _, ownerAndRepo := range repos {
+		t := strings.SplitN(ownerAndRepo, "/", 2)
 		if len(t) != 2 {
-			log.Printf("%s is malformed.", owner_repo)
+			log.Printf("%s is malformed.", ownerAndRepo)
 		}
 		owner := t[0]
 		repo := t[1]
 
 		listOpts := &github.IssueListByRepoOptions{State: "all"}
-		issues, _, err := client.Issues.ListByRepo(ctx, owner, repo, listOpts)
+		ghissues, _, err := client.Issues.ListByRepo(ctx, owner, repo, listOpts)
 		if err != nil {
 			log.Printf("Error getting issues for %s: %v", repo, err)
 			continue
 		}
-		for _, issue := range issues {
-			fmt.Printf("#%d(%s): %s\n", *issue.Number, *issue.State, *issue.Title)
-			fmt.Println(issue)
-			fmt.Println()
+		for _, issue := range ghissues {
+			i := newItem(issue, ownerAndRepo)
+			// Only report on closed issues/PRs
+			if i.State != "closed" {
+				continue
+			}
+			if i.PR {
+				prs[i.ID] = i
+			} else {
+				issues[i.ID] = i
+			}
 		}
+	}
+
+	// Print output in markdown fragments
+	fmt.Println()
+	fmt.Println("## PRs:")
+	var prkeys []string
+	for k := range prs {
+		prkeys = append(prkeys, k)
+	}
+	sort.Strings(prkeys)
+	for _, pr := range prkeys {
+		fmt.Printf("- %s\n", prs[pr])
+	}
+
+	fmt.Println()
+	fmt.Println("## Issues:")
+	var issuekeys []string
+	for k := range issues {
+		issuekeys = append(issuekeys, k)
+	}
+	sort.Strings(issuekeys)
+	for _, issue := range issuekeys {
+		fmt.Printf("- %s\n", issues[issue])
+	}
+
+	fmt.Println()
+	for _, pr := range prs {
+		fmt.Println(pr.Link())
+	}
+	for _, issue := range issues {
+		fmt.Println(issue.Link())
+	}
+	for _, user := range users {
+		fmt.Println(user.Link())
 	}
 }
